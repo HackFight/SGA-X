446112fe4d1c2e1bf6899676be10e86ed1868e6e
HackFight <hi.hugo.cornu@hotmail.com>
2025-08-07 11:54:14 +0200

    Modifs Hugo

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,19 +6,24 @@ public class LevelManager : MonoBehaviour
     InputAction menuAction;
     ItemManager itemManager;
 
-    public PlayerController player;
+    [SerializeField] PlayerController player;
     public int currentLevel = 0;
+    [SerializeField] float reloadCooldown;
+    float lastReloadTime;
 
     void Start()
     {
         menuAction = InputSystem.actions.FindAction("Menu");
         itemManager = GetComponent<ItemManager>();
+
+        lastReloadTime = Time.time;
     }
 
     void Update()
     {
-        if (menuAction.IsPressed())
+        if (menuAction.IsPressed() && Time.time > lastReloadTime + reloadCooldown)
         {
+            lastReloadTime = Time.time;
             itemManager.ReloadList(currentLevel);
             player.Respawn();
         }
